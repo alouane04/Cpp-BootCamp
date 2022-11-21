34af9f6e1fad6cdb693d1b8e0a6f39819e7f9775
alouane04 <ariahi@student.1337.ma>
2022-11-21 16:39:59 +0100

    done

--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 09:46:21 by ariahi            #+#    #+#             */
-/*   Updated: 2022/11/17 16:56:20 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/11/21 16:06:06 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,28 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
 private:
-	int					num;
+	int					n;
 	static const int	bits = 8;
 public:
 	Fixed(void);
 	Fixed(const Fixed &f);
+	Fixed(const int i);
+	Fixed(const float num);
 	Fixed& operator=(const Fixed &other);
 	~Fixed(void);
 
 	void	setRawBits( int const num );
 	int		getRawBits( void ) const;
+
+	float	toFloat(void) const;
+	int		toInt(void) const;
 };
+
+std::ostream & operator<<(std::ostream & o,const Fixed& obj);
 
 #endif
