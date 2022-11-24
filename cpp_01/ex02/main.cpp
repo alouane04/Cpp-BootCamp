8d2bd90674f91a967a4a614732d92276ce0442af
alouane04 <ariahi@student.1337.ma>
2022-11-24 09:53:29 +0100

    done

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 13:57:00 by ariahi            #+#    #+#             */
-/*   Updated: 2022/11/12 18:23:41 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/11/24 09:53:15 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int main(void)
 {
 	std::string str = "HI THIS IS BRAIN";
-	
+
 	std::string* stringPTR = &str;
-	
+
 	std::string& stringREF = str;
-	
+
 	std::cout << "_____________________ADDRESS________________________" << std::endl << std::endl;
 
 	std::cout << "The memory address of str: " << &str << std::endl << std::endl;
