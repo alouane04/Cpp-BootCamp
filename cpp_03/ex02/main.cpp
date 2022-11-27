dd513b8e0cccc721c9374853263ab9dd07a5d4ed
alouane04 <ariahi@student.1337.ma>
2022-11-27 19:00:38 +0100

    done

--- cpp_03/ex02/main.cpp
+++ cpp_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:29:38 by ariahi            #+#    #+#             */
-/*   Updated: 2022/11/27 18:23:02 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/11/27 18:50:55 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main()
 {
 	FragTrap	frag("alouane04");
 	
-	frag.attack("me");
+	frag.attack("another_alouane04");
 	frag.takeDamage(10);
 	frag.beRepaired(10);
 	frag.highFivesGuys();
