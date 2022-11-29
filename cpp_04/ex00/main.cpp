60cb70b16f6e4d938f2074317d344171553f58db
alouane04 <ariahi@student.1337.ma>
2022-11-29 20:41:58 +0100

    done

---
+++ cpp_04/ex00/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 08:53:32 by ariahi            #+#    #+#             */
+/*   Updated: 2022/11/29 18:58:34 by ariahi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	delete	meta;
+	delete	i;
+	delete	j;
+
+	const WrongAnimal* wrong_meta = new WrongAnimal();
+	const WrongAnimal* wrong_i = new WrongCat();
+
+	wrong_i->makeSound();
+	wrong_meta->makeSound();
+
+	delete	wrong_meta;
+	delete	wrong_i;
+	return 0;
+}
