636c58fa7ab25c784bd4fde7bff0147c7803f6f4
alouane04 <ariahi@student.1337.ma>
2022-12-01 23:32:26 +0100

    done

--- cpp_05/ex00/Bureaucrat.hpp
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:03:15 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/01 11:56:32 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/01 23:28:43 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,31 @@ private:
 	const std::string	name;
 	int					grade;
 public:
-	Bureaucrat(/* args */);
+	Bureaucrat();
+	Bureaucrat(std::string Name, int grade);
 	Bureaucrat(const Bureaucrat& other);
 	~Bureaucrat();
 
 	Bureaucrat& operator=(const Bureaucrat& other);
 
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw() { return "Grade is too high";}
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw() { return "Grade is too low";}
+	};
+
 	const std::string	getName() const;
-	int	getGrade() const;
+	int					getGrade() const;
+	void				incrementGrade();
+	void				decrementGrade();
 };
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& other);
 
 #endif
