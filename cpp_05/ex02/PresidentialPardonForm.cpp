6045fec3a9a98304a8b835ada05a39c2275920c1
alouane04 <ariahi@student.1337.ma>
2022-12-03 16:33:47 +0100

    done

--- cpp_05/ex02/PresidentialPardonForm.cpp
+++ cpp_05/ex02/PresidentialPardonForm.cpp
@@ -6,16 +6,38 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 21:47:00 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/02 21:47:12 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/03 13:28:40 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PresidentialPardonForm.hpp"
 
-PresidentialPardonForm::PresidentialPardonForm(/* args */)
+PresidentialPardonForm::PresidentialPardonForm()
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string& Target) : target(Target), Form("PresidentialPardonForm", 25, 5)
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other) : target(other.target), Form(other)
 {
 }
 
 PresidentialPardonForm::~PresidentialPardonForm()
 {
+}
+
+PresidentialPardonForm&	PresidentialPardonForm::operator=(const PresidentialPardonForm& other)
+{
+	(void)other;
+	return (*this);
+}
+
+void	PresidentialPardonForm::execute(const Bureaucrat& executer) const
+{
+	if (executer.getGrade() > this->getExeGrade())
+		throw Form::GradeTooLowException();
+	else
+		std::cout << this->target << " has been pardoned by Zaphod Beeblebrox" << std::endl;
 }
--- cpp_05/ex02/PresidentialPardonForm.hpp
+++ cpp_05/ex02/PresidentialPardonForm.hpp
@@ -6,21 +6,28 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 21:44:59 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/02 21:46:34 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/03 12:42:48 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRESIDENTIALPARDONFORM_HPP
 #define PRESIDENTIALPARDONFORM_HPP
 
-class PresidentialPardonForm
+#include "Form.hpp"
+
+class PresidentialPardonForm : public Form
 {
 private:
-	/* data */
+	const std::string	target;
 public:
-	PresidentialPardonForm(/* args */);
+	PresidentialPardonForm();
+	PresidentialPardonForm(const std::string& Target);
 	PresidentialPardonForm(const PresidentialPardonForm& other);
 	~PresidentialPardonForm();
+
+	PresidentialPardonForm&	operator=(const PresidentialPardonForm& other);
+
+	void	execute(const Bureaucrat& executor) const;
 };
 
 #endif
