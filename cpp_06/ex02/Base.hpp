a736d56c5eebf413a036d7087f54092295e403f3
alouane04 <ariahi@student.1337.ma>
2022-12-05 20:10:06 +0100

    done

--- cpp_06/ex02/Base.hpp
+++ cpp_06/ex02/Base.hpp
@@ -6,12 +6,15 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 16:58:18 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/05 17:06:04 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/05 20:06:32 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 #define	BASR_HPP
+
+#include "iostream"
+#include <random>
 
 class Base
 {
--- cpp_06/ex02/main.cpp
+++ cpp_06/ex02/main.cpp
@@ -6,8 +6,39 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 16:58:02 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/05 16:58:05 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/05 20:05:38 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "Base.hpp"
+
+int main()
+{
+	Base*	a = generate();
+	Base*	b = generate();
+	Base*	c = generate();
+
+	std::cout << "by addresse:" << std::endl;
+
+	std::cout << "a = ";
+	identify(a);
+	
+	std::cout << "b = ";
+	identify(b);
+
+	std::cout << "c = ";
+	identify(c);
+
+	std::cout << "by reference:" << std::endl;
+
+	std::cout << "a = ";
+	identify(a);
+
+	std::cout << "b = ";
+	identify(b);
+
+	std::cout << "c = ";
+	identify(b);
+	
+	return (delete a, delete b, delete c, 0);
+}
