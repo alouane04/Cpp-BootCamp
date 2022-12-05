ce523dbe535d134ee95734100b5c4c63c6c39a52
alouane04 <ariahi@student.1337.ma>
2022-12-05 12:24:48 +0100

    done

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 16:59:28 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/04 17:00:24 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/05 12:12:28 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 int main(int ac, char **av)
 {
-	
+	if (ac != 2)
+		return (std::cout << "Invalid num args" << std::endl, 1);
+	if (av[1])
+	{
+		Cast	cast(av[1]);
+		try
+		{
+			cast.check();
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
 	return (0);
 }
