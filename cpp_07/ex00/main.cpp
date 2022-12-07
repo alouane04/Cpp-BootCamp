005ea065983f51a7fd38efc0b07046662d31f158
alouane04 <ariahi@student.1337.ma>
2022-12-07 19:00:50 +0100

    done

---
+++ cpp_07/ex00/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 15:36:31 by ariahi            #+#    #+#             */
+/*   Updated: 2022/12/07 19:00:38 by ariahi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+template <typename T> void	swap(T a, T b)
+{
+	
+}
+
+template <typename T> T		min(T a, T b)
+{
+	
+}
+
+template <typename T> T		max(T a, T b)
+{
+	
+}
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 15:37:23 by ariahi            #+#    #+#             */
+/*   Updated: 2022/12/07 18:58:53 by ariahi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include "iostream"
+
+template	<typename T> void	swap(T a, T b);
+
+template	<typename T> T	min(T a, T b);
+
+template	<typename T> T	max(T a, T b);
+
+#endif
