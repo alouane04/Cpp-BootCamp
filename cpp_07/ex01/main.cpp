88db98610d9fe323620523c766a468783c355f76
alouane04 <ariahi@student.1337.ma>
2022-12-08 20:36:05 +0100

    done

--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 14:12:29 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/08 15:52:19 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/08 19:14:50 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 int main()
 {
 	int		array[5] = {1, 2, 3, 4, 5};
-	int		len = sizeof(array) / 4;
+	int		len = sizeof(array) / sizeof(array[0]);
 
 	char	array2[5] = {'a', 'b', 'c', 'd', 'e'};
 	int		len2 = sizeof(array2);
 
 	float	array3[5] = {1.3, 2.4, 3.6, 4.6, 5.7};
-	int		len3 = sizeof(array3) / 4;
+	int		len3 = sizeof(array3) / sizeof(array[0]);
 
 	iter(array, len, &show);
 	iter(array2, len2, &show);
