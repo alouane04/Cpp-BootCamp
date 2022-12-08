9ab98f74afbf199d72fddd1acdfee7719a3717c8
alouane04 <ariahi@student.1337.ma>
2022-12-08 15:56:23 +0100

    done

---
+++ cpp_07/ex01/iter.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 14:12:59 by ariahi            #+#    #+#             */
+/*   Updated: 2022/12/08 15:53:33 by ariahi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include "iostream"
+#include "string.h"
+
+template	<typename T> 
+void	iter(T *array, int len, void (*f)(T &f))
+{
+	int	i;
+
+	i = -1;
+	while (len > ++i)
+		if (array[i])
+			f(array[i]);
+	std::cout << std::endl;
+}
+
+template	<typename T>
+void	show(T array)
+{
+	std::cout << array << " ";
+}
+
+#endif
