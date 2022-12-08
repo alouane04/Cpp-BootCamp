115110d3854228a7382471c08edd8bc5b2e892ec
alouane04 <ariahi@student.1337.ma>
2022-12-08 14:11:45 +0100

    done

--- cpp_07/ex00/whatever.hpp
+++ cpp_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 15:37:23 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/07 18:58:53 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/08 14:10:59 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,30 @@
 
 #include "iostream"
 
-template	<typename T> void	swap(T a, T b);
+template	<typename T> void	swap(T &a, T &b);
 
 template	<typename T> T	min(T a, T b);
 
 template	<typename T> T	max(T a, T b);
+
+
+template <typename T> void	swap(T &a, T &b)
+{
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T> T		min(T a, T b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T> T		max(T a, T b)
+{
+	return (a < b ? b : a);
+}
 
 #endif
