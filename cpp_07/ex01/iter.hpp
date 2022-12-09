9b13645e6bee425339cadd351e33b062a0dbf340
alouane04 <ariahi@student.1337.ma>
2022-12-09 09:11:58 +0100

    done

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 14:12:59 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/08 15:56:54 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/09 09:02:42 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	iter(T *array, int len, void (*f)(T &f))
 
 	i = -1;
 	while (len > ++i)
-		if (array[i])
 			f(array[i]);
 	std::cout << std::endl;
 }
