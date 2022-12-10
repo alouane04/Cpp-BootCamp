f0c94fa1bf429d8431cceafb7ff7f43531775e9a
alouane04 <ariahi@student.1337.ma>
2022-12-10 22:28:15 +0100

    done

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 12:33:49 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/09 12:49:03 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/10 22:08:32 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 #define EASYFIND_HPP
 
 #include "iostream"
+#include <vector>
+#include <algorithm>
 
 template <typename T>
-void	easyfind(T arr, int toFind)
+void	easyfind(T &vec, int toFind)
 {
-	
+	if (std::find(vec.begin(), vec.end(), toFind) != vec.end())
+		std::cout << "found" << std::endl;
+	else
+		throw std::out_of_range ("not found");
 }
 
 #endif
