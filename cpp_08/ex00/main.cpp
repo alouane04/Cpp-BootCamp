d2ae22e684be115fefd3b41e924397404d9684d9
alouane04 <ariahi@student.1337.ma>
2022-12-10 22:28:20 +0100

    done

--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,25 +6,22 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 12:33:04 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/09 18:57:58 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/10 22:26:52 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "easyfind.hpp"
+#include "easyfind.hpp"
 #include <iostream>
 #include <vector>
-using namespace std;
+
 int main()
 {
-    vector<int> ar = { 1, 2, 3, 4, 5 };
-      
-    // Declaring iterator to a vector
-    vector<int>::iterator ptr;
-      
-    // Displaying vector elements using begin() and end()
-    cout << "The vector elements are : ";
-    for (ptr = ar.begin(); ptr < ar.end(); ptr++)
-        cout << *ptr << " ";
-      
-    return 0;    
+    int array[] = {1, 2, 3, 4, 5};
+
+    std::vector<int>    vec(array, array + sizeof(array) / sizeof(int));
+
+    easyfind(vec, 1);
+    easyfind(vec, 10);
+
+    return (0);    
 }
