4dd2438b38741bd298812f015d1c14fff549a390
alouane04 <ariahi@student.1337.ma>
2022-12-11 18:56:53 +0100

    done

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 22:37:14 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/10 22:37:30 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/11 18:47:34 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int main()
 {
-	
+	Span sp = Span(5);
+
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+	return 0;
 }
