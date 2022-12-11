e8002b9ace423e0b2721d7cf18504269e8eaffca
alouane04 <ariahi@student.1337.ma>
2022-12-11 18:57:03 +0100

    done

--- cpp_08/ex01/Span.hpp
+++ cpp_08/ex01/Span.hpp
@@ -6,20 +6,36 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 22:37:36 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/10 22:38:09 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/11 18:55:18 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include "iostream"
+#include <vector>
+#include <algorithm>
+
 class Span
 {
 private:
-	/* data */
+	std::vector<int>	vec;
+	unsigned int		N;
 public:
-	Span(/* args */);
+	Span();
+	Span(unsigned int n);
+	Span(const Span& other);
 	~Span();
+
+	Span&	operator=(const Span& other);
+	
+	void	addNumber(const int n);
+	int		shortestSpan();
+	int		longestSpan();
+
+	void	rangeOfIterators(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+	
 };
 
 #endif
