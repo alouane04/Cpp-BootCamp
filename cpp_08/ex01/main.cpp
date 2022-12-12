2bfd7b78c96211034d8fcd1f69a323a7fed3570b
alouane04 <ariahi@student.1337.ma>
2022-12-12 16:16:36 +0100

    done

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 22:37:14 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/11 18:47:34 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/12/12 16:15:10 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,43 @@
 
 int main()
 {
-	Span sp = Span(5);
+	{
+		Span sp = Span(5);
 
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		//sp.addNumber(111);
+		//sp.addNumber(1);
+		
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
 
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	{
+		std::cout << "Test 2" << std::endl;
+		try{
+		std::vector<int>	vec(100000);
+		std::srand(time(NULL));
+		std::generate(vec.begin(), vec.end(), std::rand);
+		Span sp = Span(vec.size()); 
 
+		// for (std::vector<int>::iterator i = vec.begin(); i < vec.end(); ++i) //To see inside the span
+		// 	std::cout << *i << " ";
+
+		sp.rangeOfIterators(vec.begin(), vec.end());
+		
+		//sp.addNumber(1); //To test the limits
+		
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
 	return 0;
 }
